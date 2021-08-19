b66f5fa3388187979074bf2bf919842749465237
Nicod Gaetan <nicod.ga@gmail.com>
2021-08-19 11:20:36 +0200

    leaflet version

---
+++ lib/hotel_booking/map_view.dart
@@ -0,0 +1,85 @@
+import 'package:best_flutter_ui_templates/hotel_booking/hotel_app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'model/hotel_list_data.dart';
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+
+class MapView extends StatelessWidget {
+  static const String route = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    var markers = <Marker>[
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(51.5, -0.09),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            textColor: Colors.blue,
+            key: ObjectKey(Colors.blue),
+          ),
+        ),
+      ),
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(53.3498, -6.2603),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            textColor: Colors.green,
+            key: ObjectKey(Colors.green),
+          ),
+        ),
+      ),
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(48.8566, 2.3522),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            textColor: Colors.purple,
+            key: ObjectKey(Colors.purple),
+          ),
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      body:Column(
+          children: [
+            Flexible(
+              child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(51.5, -0.09),
+                  zoom: 5.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate:
+                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    subdomains: ['a', 'b', 'c'],
+                    // For example purposes. It is recommended to use
+                    // TileProvider with a caching and retry strategy, like
+                    // NetworkTileProvider or CachedNetworkTileProvider
+                    tileProvider: NonCachingNetworkTileProvider(),
+                  ),
+                  MarkerLayerOptions(markers: markers)
+                ],
+              ),
+            ),
+          ],
+      ),
+    );
+  }
+}
+
